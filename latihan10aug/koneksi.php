089c5e530f687f2a70185c4892a55b63274db15c
Ridwan Annafi <98511080+RidwanAnnV@users.noreply.github.com>
2022-08-10 12:03:08 +0700

    Add files via upload

---
+++ latihan10aug/koneksi.php
@@ -0,0 +1,18 @@
+<?php
+$kn = new PDO('mysql:host=localhost;dbname=ppdb','root','');
+
+$query = $kn->query("UPDATE calonsiswa SET asal='SMP MUHAMMADIYAH' WHERE id=4725");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LATIHAN 10 AGUSTUS</title>
+</head>
+<body>
+    
+</body>
+</html>
